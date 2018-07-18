84df4fa52f138af544a8f775380a84f499a1a235
Erica Sivak <ernoone87@gmail.com>
2018-07-18 13:32:31 -0700

    Updated version number

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -14,7 +14,7 @@
 
 pkg_name=very-fun-node-app
 pkg_origin=habitup
-pkg_version="1.1.1"
+pkg_version="1.1.2"
 pkg_deps=(core/node)
 
 # Habitat provides you with a number of built-in "callbacks" to use
